50a58a51f76fa7aab31406630bc1a3ea17025803
Alejandro Toledo <atoledofr@gmail.com>
2022-01-10 09:35:16 -0500

    delete and comment the ver mas button

--- src/components/buttons/ver_mas.js
+++ src/components/buttons/ver_mas.js
@@ -5,8 +5,8 @@ import { recordCreateAction } from '../../redux/actions/recordsActions';
 
 const Vermas = ({recordCreateAction}) => {
   return (
-    <div >
-      <Dropdown className="rounded flex content-around my-2 mx-2 px-2 py-2 text-indigo-400">
+    <>
+      {/* <Dropdown className="rounded flex content-around my-2 mx-2 px-2 py-2 text-indigo-400">
         <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
         <i className="fas fa-info-circle"></i>&nbsp; Ver más
         </Dropdown.Toggle>
@@ -18,11 +18,11 @@ const Vermas = ({recordCreateAction}) => {
             </svg>
             &nbsp; Guardar
           </Dropdown.Item>
-          {/* <Dropdown.Item href="#/action-3" className="flex py-1">
+          <Dropdown.Item href="#/action-3" className="flex py-1">
             <i class="fab fa-whatsapp"> </i>
             &nbsp;  Whatsapp
-          </Dropdown.Item> */}
-        {/* <Dropdown.Item href="#/action-2" className="flex py-1">
+          </Dropdown.Item>
+        <Dropdown.Item href="#/action-2" className="flex py-1">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
           <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
           </svg>
@@ -32,16 +32,11 @@ const Vermas = ({recordCreateAction}) => {
           <i class="fab fa-whatsapp"> </i>
           &nbsp; Contacto Whatsapp
         </Dropdown.Item>
-        </Dropdown.Menu> */}
-        {/* </Dropdown>  */}
+        </Dropdown.Menu> */
         </Dropdown.Menu>
-      </Dropdown> 
-    </div>
+      </Dropdown>  */}
+    </>
   )
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  recordCreateAction: (arId) => dispatch(recordCreateAction(arId)),
-});
-
-export default connect(mapDispatchToProps)(Vermas);
+export default Vermas;
